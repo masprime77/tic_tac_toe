fa6b0415a55eba320fb1ee57eb61da47cbfee18c
Mateo Arenas <masprime77@gmail.com>
2024-12-09 13:17:44 +0100

    board.py prints a primitive board

---
+++ board.py
@@ -0,0 +1,10 @@
+size = 6
+
+for i in range(3):
+    print("+" + "-" * size * 3 + "+")
+    for j in range(3):
+        print("|     ", end="")
+
+    print("|")
+
+print ("+" + "-" * size * 3 + "+")
