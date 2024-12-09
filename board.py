f41aa9866af8a66c22a447314a6ce209b7d106a5
Mateo Arenas <masprime77@gmail.com>
2024-12-09 21:12:12 +0100

    board.py prints a board

--- board.py
+++ board.py
@@ -1,10 +1,5 @@
-size = 6
-
 for i in range(3):
-    print("+" + "-" * size * 3 + "+")
-    for j in range(3):
-        print("|     ", end="")
+    print("+-----+-----+-----+\n" +
+          "|     |     |     |")
 
-    print("|")
-
-print ("+" + "-" * size * 3 + "+")
+print("+-----+-----+-----+")
