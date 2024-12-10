f84118c362a20df8dadc7f454144b0f48e8f5c3f
Mateo Arenas <masprime77@gmail.com>
2024-12-10 21:21:58 +0100

    game.py is the new main code. It is intended to run the complete game.

---
+++ game.py
@@ -0,0 +1,6 @@
+def main():
+    board = [[" " for i in range(3)] for j in range(3)] # Generates a double list 3x3 with whitespaces.
+    current_player = "X"
+
+if __name__ == '__main__':
+    main()
