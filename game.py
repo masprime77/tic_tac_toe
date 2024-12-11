658a8269d7bff2ea76dffcb9bd0a7474ab507228
Mateo Arenas <masprime77@gmail.com>
2024-12-11 15:57:04 +0100

    Added executable for Mac and Linux. Now it's renamed.
    Move code in check_winner method. For some cases, the game didn't end

--- game.py
+++ game.py
@@ -9,15 +9,15 @@ def check_winner(board):
         if board[0][i] == board[1][i] == board[2][i] != " ":
             return board[0][i]
 
-        if board[0][0] == board[1][1] == board[2][2] != " ":
-            return board[0][0]
-        if board[0][2] == board[1][1] == board[2][0] != " ":
-            return board[0][2]
+    if board[0][0] == board[1][1] == board[2][2] != " ":
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != " ":
+        return board[0][2]
 
-        if all(cell != " " for row in board for cell in row):
-            return "Draw"
+    if all(cell != " " for row in board for cell in row):
+        return "Draw"
 
-        return None
+    return None
 
 def print_board(board):
     os.system('clear')
